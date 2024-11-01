912baaed0f77f0924629716b31c6589a4d37470f
Laurent Meuwly <laurent.meuwly@radiofr.ch>
2024-11-01 21:32:04 +0100

    Force to display Yes or No for boolean var

--- resources/views/mail/contact-email.blade.php
+++ resources/views/mail/contact-email.blade.php
@@ -3,8 +3,8 @@ Voici les détails du Contact qui a été généré depuis votre site Internet:
 Prénom : {{ $contact->firstname }}<br>
 Nom : {{ $contact->lastname }}<br>
 Email : {{  $contact->email }}<br>
-Newsletter : {{  $contact->newsletter }}<br>
-Consentement donné : {{  $contact->agreement }}<br>
+Newsletter : {{  $contact->newsletter ? 'oui' : 'non' }}<br>
+Consentement donné : {{  $contact->agreement ? 'oui' : 'non'  }}<br>
 <br>
 Message :<br>
 {{ $contact->message }}
